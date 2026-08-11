9fb6371941fc37f723e71ee8345b1cd8e5516323
DevendraYadav <devendracp23@gmail.com>
2026-08-11 10:15:06 +0530

    complete the basic of python

--- module-03-python-programming/fundamentals/main.py
+++ module-03-python-programming/fundamentals/main.py
@@ -5,3 +5,33 @@ print("I am learning Python");
 print("I am learning Python in CDAC course");
 
 print(10 + 20);
+
+
+# variable in python
+
+x=10   
+print(x)
+
+
+
+# Data types in python
+x=10
+print(x)
+
+name = "Devendra"
+print(name)
+
+y= 10.5
+print(y)
+
+
+# type()--  type() tells you the type of a value.
+
+age = 25
+
+print(type(age))
+
+# taking input from user
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
