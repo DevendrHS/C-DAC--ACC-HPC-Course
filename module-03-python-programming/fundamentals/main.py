5ba13d38bc43b034a9f79c35db813f13078dd4bf
DevendraYadav <devendracp23@gmail.com>
2026-08-11 09:55:04 +0530

    learning syntax  of python

--- module-03-python-programming/fundamentals/main.py
+++ module-03-python-programming/fundamentals/main.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+#  learning syntax of python programming
+print("Hello, World!");
+print("My name is Devendra");
+print("I am learning Python");
+print("I am learning Python in CDAC course");
+
+print(10 + 20);
