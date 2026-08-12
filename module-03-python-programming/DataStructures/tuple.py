cffb339961c81722d43258c64674c5ebdfa5ff86
DevendraYadav <devendracp23@gmail.com>
2026-08-12 11:55:18 +0530

    complete tuple

---
+++ module-03-python-programming/DataStructures/tuple.py
@@ -0,0 +1,16 @@
+#Tuple-- immutable data type in Python
+
+
+data = (10,20, 20, 30, "python", 40, True, 3.14, 50)
+print(data)
+
+data[0] = 100  # This will raise an error because tuples are immutable
+print(data)
+
+
+
+
+# difference between list and tuple
+# () - tuple
+# [] - list
+#tuple is immutable, list is mutable
