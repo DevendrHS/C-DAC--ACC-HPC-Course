cf45b2cef9a621c92c4c0a84f70a3093e74f9265
DevendraYadav <devendracp23@gmail.com>
2026-08-13 10:20:02 +0530

    complete sets in python

---
+++ module-03-python-programming/DataStructures/sets.py
@@ -0,0 +1,16 @@
+# sets--store unique values in a single variable
+# Sets are mutable data types in Python.
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+
+data = {10,10, 20, 30, "python", 40, True, 3.14, 50}# print only unique values
+print(data)
+
+
+## Adding an element to the set
+data.add("Devendra")
+print(data)
+
+# set does not support indexing, slicing, or other sequence-like behavior.
---
+++ module-03-python-programming/Numpy/arrays.py
@@ -0,0 +1,7 @@
+# from numpy arrays
+# ways of creating arrays in numpy
+import array
+
+
+arr = array([1, 2, 3, 4, 5],[1, 2, 3, 4, 5])
+print(arr)    
