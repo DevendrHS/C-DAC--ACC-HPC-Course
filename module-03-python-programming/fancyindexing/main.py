19a3765cf580b8e6d2c152b81225ff6442bfbfe0
DevendraYadav <devendracp23@gmail.com>
2026-08-21 23:56:35 +0530

    start high-speed-interconnect

---
+++ module-03-python-programming/fancyindexing/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#Fancy indexing means selecting elements from a sequence using multiple indexes at once,
+#  rather than selecting just one position or a continuous slice.
+
+
+
+
